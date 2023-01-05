c53493f67618a8b95d3a7b3a4e65e63a1ccbced5
Hseongju <sj951002@gmail.com>
2023-01-05 14:58:07 +0900

    팝업 테스트중

--- common/js/script.js
+++ common/js/script.js
@@ -54,7 +54,7 @@ var commonScript = (function(){
               lazyLoading();
               setTimeout(function(){
                 popupResize();
-              },100)
+              },150)
             }
           }
         });
